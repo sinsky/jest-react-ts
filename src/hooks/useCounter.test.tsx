5e260280f4c98cec92691bf2e2427250b82f93e9
sinsky <60662802+sinsky@users.noreply.github.com>
2024-01-03 22:47:01 +0900

    test: create useCounter test

---
+++ src/hooks/useCounter.test.tsx
@@ -0,0 +1,17 @@
+import { act, renderHook } from "@testing-library/react";
+import useCounter from "./useCounter";
+
+describe("useCounter", () => {
+  it("increment", () => {
+    const { result } = renderHook(() => useCounter(1));
+    expect(result.current.count).toBe(1);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(2);
+  });
+  it("decrement", () => {
+    const { result } = renderHook(() => useCounter(1));
+    expect(result.current.count).toBe(1);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(0);
+  });
+});
